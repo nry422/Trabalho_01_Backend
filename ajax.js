1188be714ae2d94ca675b4da4168dc533fcc7c04
nry422 <henrymuller@gmail.com>
2026-05-12 23:40:13 -0300

    jquery, correções
    ajax.js agora é todo em jquery
    algumas modificações e experimentações no index para melhorar comportamento do bootstrap e aparencia,

--- ajax.js
+++ ajax.js
@@ -1,4 +1,5 @@
-//aqui é jquery
+const botaoAtualizar = $("#botao-atualizar");
+
 function listarContatos(){
     $.get("api.php", { acao: "listar"}, function(contatos) {
         let linhas = "";
@@ -9,8 +10,10 @@ function listarContatos(){
             linhas += "<td>" + c.nome + "</td>";
             linhas += "<td>" + c.telefone + "</td>";
             linhas += "<td>" + c.email + "</td>";
-            linhas += "<td><button type='button' onclick='preencherFormulario(" + chave + ", \"" + c.nome + "\", \"" + c.telefone + "\", \"" + c.email + "\")' class='btn btn-primary btn-sm'>Editar</button></td>";
-            linhas += "<td><button type='button' onclick='excluirContato(" + chave + ")' class='btn btn-danger btn-sm'>Excluir</button></td>";
+            linhas += "<td>";
+            linhas += "<button type='button' onclick='preencherFormulario(" + chave + ", \"" + c.nome + "\", \"" + c.telefone + "\", \"" + c.email + "\")' class='btn btn-primary btn-sm me-1'>Editar</button> ";
+            linhas += "<button type='button' onclick='excluirContato(" + chave + ")' class='btn btn-danger btn-sm'>Excluir</button>";
+            linhas += "</td>";
             linhas += "</tr>";
         }
         $("#tabela-contatos").html(linhas);
@@ -30,148 +33,102 @@ $("form").on("keypress", function(e) { //para poder usar enter para tivar o bota
     }
 });
 
+function inserirContato(){
+    const nome = $("#nome").val();
+    const telefone = $("#telefone").val();
+    const email = $("#email").val();
 
-
-//daqui para baixo ainda é JS TIRANDO O VERIFICARCAMPOS
-const botaoAtualizar = document.getElementById("botao-atualizar"); //para 
-
-function inserirContato() {
-    const nome     = document.getElementById("nome").value;
-    const telefone = document.getElementById("telefone").value;
-    const email    = document.getElementById("email").value;
-    const xmlhttp = new XMLHttpRequest();
-    xmlhttp.onload = function () {
-        const resposta = JSON.parse(this.responseText);
-        if (resposta.status == "ok") {
+    $.post("api.php", {
+        acao: "inserir",
+        nome: nome,
+        telefone: telefone,
+        email: email
+    }, function(resposta) {
+        if (resposta.status == "ok"){
             alert("Contato salvo!");
-            limparFormulario()
+            limparFormulario();
             listarContatos();
         }
     }
-    xmlhttp.open("POST", "api.php");
-    xmlhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-    xmlhttp.send("acao=inserir&nome=" + nome + "&telefone=" + telefone + "&email=" + email);
+);
 }
 
 function atualizarContato() {
-    const id       = document.getElementById("id").value;
-    const nome     = document.getElementById("nome").value;
-    const telefone = document.getElementById("telefone").value;
-    const email    = document.getElementById("email").value;
-    const xmlhttp = new XMLHttpRequest();
-    xmlhttp.onload = function () {
-        const resposta = JSON.parse(this.responseText);
+    const id = $("#id").val();
+    const nome = $("#nome").val();
+    const telefone = $("#telefone").val();
+    const email = $("#email").val();
+
+    $.post("api.php", {
+        acao: "atualizar",
+        id: id,
+        nome: nome,
+        telefone: telefone,
+        email: email
+    }, function(resposta){
         if (resposta.status == "ok") {
             alert("Contato atualizado!");
-            limparFormulario()
+            limparFormulario();
             listarContatos();
         }
     }
-    xmlhttp.open("POST", "api.php");
-    xmlhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-    xmlhttp.send("acao=atualizar&id=" + id + "&nome=" + nome + "&telefone=" + telefone + "&email=" + email);
+);
 }
 
 function excluirContato(id) {
-    const xmlhttp = new XMLHttpRequest();
-    xmlhttp.onload = function () {
-        const resposta = JSON.parse(this.responseText);
+    $.post("api.php", {
+        acao: "excluir",
+        id: id
+    }, function(resposta) {
         if (resposta.status == "ok") {
             alert("Contato excluído!");
             listarContatos();
         }
     }
-    xmlhttp.open("POST", "api.php");
-    xmlhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-    xmlhttp.send("acao=excluir&id=" + id);
+);
 }
 
-function preencherFormulario(id, nome, telefone, email) {
-    document.getElementById("id").value       = id;
-    document.getElementById("nome").value     = nome;
-    document.getElementById("telefone").value = telefone;
-    document.getElementById("email").value    = email;
+function preencherFormulario(id, nome, telefone, email){
+    $("#id").val(id);
+    $("#nome").val(nome);
+    $("#telefone").val(telefone);
+    $("#email").val(email);
 
-    //esse para o botão aparecer e sumir com ele
-    botaoAtualizar.classList.remove('d-none');
+    //para o botao aparecer e sumir com ele
+    botaoAtualizar.removeClass('d-none');
 }
 
-//esse para tirar as coisas que tem no form
-function limparFormulario() {
-    document.getElementById("id").value = "";
-    document.getElementById("nome").value = "";
-    document.getElementById("telefone").value = "";
-    document.getElementById("email").value = "";
-    document.getElementById("nome").focus();
-    
-    botaoAtualizar.classList.add('d-none');
-    $("#botao-limpar").addClass('d-none'); //isso é jquery também, ele esconde o botao após limpar
+function limparFormulario(){
+    $("#id").val("");
+    $("#nome").val("");
+    $("#telefone").val("");
+    $("#email").val("");
+    $("#nome").focus();
+
+    botaoAtualizar.addClass('d-none');
+    $("#botao-limpar").addClass('d-none');
 
 }
 
-// é JQUERY! ele verifica se você digitou algo e mostra o botao limpar, não se se deveria manter isso...
+//verifica se você digitou algo e mostra o botao limpar, não se se deveria manter isso...
 function verificarCampos() {
-    const nome     = $("#nome").val();
-    const telefone = $("#telefone").val();
-    const email    = $("#email").val();
-
-    if (nome || telefone || email) {
+    if ($("#nome").val() || $("#telefone").val() || $("#email").val()) {
         $("#botao-limpar").removeClass('d-none');
     } else {
         $("#botao-limpar").addClass('d-none');
     }
 }
 
-$("#nome").on("input", verificarCampos);
-$("#telefone").on("input", verificarCampos);
-$("#email").on("input", verificarCampos);
+$("#nome, #telefone, #email").on("input", verificarCampos);
 
-//to fazendo esta que faz a tabela aparecer somente se tiver coisa, se não tiver ela mostra a outra mensagem
 function atualizaTabela(quantidade) {
-    const tabela = document.getElementById('tabela-toda');
-    const lista = document.getElementById('tabela-contatos');
-    const mensagem = document.getElementById('mensagem-vazia')
-
-    //verifica, tira e coloca
-    if(quantidade > 0){  //ve se tem mais que nada na tabela
-        tabela.classList.remove('d-none');
-        mensagem.classList.add('d-none');
-    } else {  //se não tiver nada na tabela aí mostra a mensagemdenada
-        tabela.classList.add('d-none'); //esconde a tabela (em caso se apagar tudo)
-        mensagem.classList.remove('d-none');
+    if(quantidade > 0) { //checa se tem mais que nada na tabela
+        $("#tabela-toda").removeClass('d-none');
+        $("#mensagem-vazia").addClass('d-none');
+    } else { //se não tiver nada na tabela mostra a mensagem
+        $("#tabela-toda").addClass('d-none'); //esconde a tabela
+        $("#mensagem-vazia").removeClass('d-none');
     }
 }
 
-
 listarContatos();
-
-
-
-
-
-//versão JS do function listar contatos ainda aqui, acima está em jquery
-/*function listarContatos() {
-    const xmlhttp = new XMLHttpRequest();
-    xmlhttp.onload = function () {
-        const contatos = JSON.parse(this.responseText);
-        let linhas = "";
-        for (let chave in contatos){
-            let c = contatos[chave];
-            linhas += "<tr>";
-            linhas += "<td>" + chave + "</td>";
-            linhas += "<td>" + c.nome + "</td>";
-            linhas += "<td>" + c.telefone + "</td>";
-            linhas += "<td>" + c.email + "</td>";
-            linhas += "<td><button type='button' onclick='preencherFormulario(" + chave + ", \"" + c.nome + "\", \"" + c.telefone + "\", \"" + c.email + "\")' class='btn btn-primary btn-sm'>Editar</button></td>";
-            linhas += "<td><button type='button' onclick='excluirContato(" + chave + ")' class='btn btn-danger btn-sm'>Excluir</button></td>";
-            linhas += "</tr>";
-        }
-        document.getElementById("tabela-contatos").innerHTML = linhas;
-
-        atualizaTabela(Object.keys(contatos).length); 
-    }
-    xmlhttp.open("GET", "api.php?acao=listar");
-    xmlhttp.send();
-} */
-
-
--- index.php
+++ index.php
@@ -8,53 +8,54 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css">
     <title>Agenda de contatos com AJAX</title>
 </head>
-<!--teste para ver se é possivel vc ver responda aqui R: EU VEJO!!!!!!!!!!! :D -->
+
 <body>
 
-    <div class="container">
+    <div class="container" style="max-width: 900px">
         <h1 class="my-4"> Agenda do AJAX</h1>        
         <!--form para inserir e atualizar a tabela-->
+        <div class="card shadow-sm p-3 mb-4">
         <form>            
             <!--id fica escondido mas é necessario aqui na hora de editar-->
             <input type="hidden" id="id">
                 <div class="row g-2 align-items-end">
-                    <div class="col">
+                    <div class="col-12 col-md">
                         <label for="nome">Nome:</label>
                         <input type="text" id="nome" class="form-control" required autofocus>
                     </div>    
-                    <div class="col">
+                    <div class="col-12 col-md">
                         <label for="telefone">Telefone:</label>
                         <input type="text" id="telefone" class="form-control" required>
                     </div>
-                    <div class="col">
+                    <div class="col-12 col-md">
                         <label for="email">Email:</label>
                         <input type="text" id="email" class="form-control" required>
                     </div>
-                    <div class="col-auto">
+                    <div class="col-12 col-md-auto">
                         <button type="button" onclick="inserirContato()" class="btn btn-primary">Adicionar</button>
                         <button type="button" onclick="atualizarContato()" class="btn btn-primary d-none" id="botao-atualizar">Atualizar</button>
                         <button type="button" onclick="limparFormulario()" class="btn btn-secondary d-none" id="botao-limpar">Limpar</button>
                     </div>
         </div>
         </form>
+</div>
         
     <div class="table-responsive">
         <table class="table mt-4 d-none" id="tabela-toda"> <!--Aqui ele está com d-none pois some e aparece conforme dados no json-->
-            <thead>
+            <thead class="table-light">
                 <tr>
                     <th scope="col">ID</th>
                     <th scope="col">Nome</th>
                     <th scope="col">Telefone</th>
                     <th scope="col">Email</th>
-                    <th scope="col">Editar</th>
-                    <th scope="col">Excluir</th>
+                    <th scope="col">Ações</th>                    
                 </tr>
             </thead>
             <tbody id="tabela-contatos">           <!--pega os valores do ajax que vem do arquivo json-->
             </tbody>
         </table>
     </div>    
-         <p id="mensagem-vazia" class="mt-3">Ainda não há nenhum contato na agenda! <br> Adicione para aparecer!</p>
+         <p id="mensagem-vazia" class="mt-3 text-muted fst-italic">Ainda não há nenhum contato na agenda! <br> Adicione para aparecer!</p>
         </div>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script> 
       <script src="ajax.js"></script>         
